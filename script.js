95d67051f763308db83604c1790b073c69d5ae54
Kevin Jones <joneskj55@gmail.com>
2019-04-07 12:22:16 -0500

    added jQuery | used jQuery solution for selectedAnswer variable

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function checkAnswer() {
-    const correctAnswer = document.getElementById('correctAnswer');
-    const selectedAnswer = document.getElementsByClassName('radioButton').checked;
+    const correctAnswer = document.getElementById('correctAnswer').value;
+    const selectedAnswer = $('input[type="radio"][name="teamName"]:checked').val();
 
-    if (selectedAnswer === correctAnswer) {
-        document.getElementById('result').innerHTML = 'Great job!';
-    } else {
-        document.getElementById('result').innerHTML = 'Sorry, that is incorrect.';
-    }
+        if (selectedAnswer === correctAnswer) {
+            document.getElementById('result').innerHTML = 'Great job!';
+            console.log(correctAnswer + ' ' + selectedAnswer);
+        } else {
+            document.getElementById('result').innerHTML = 'Sorry, that is incorrect.';
+            console.log(correctAnswer + ' ' + selectedAnswer);
+        }
 }
-
